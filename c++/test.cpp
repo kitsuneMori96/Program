13ed416a327700674a8d607b6d6e1e77ef3e66bc
kitsuneMori <2794637787@qq.com>
2025-12-16 14:07:21 +0800

    feat: 添加主函数以读取输入字符串
    在`test.cpp`中添加了一个主函数`main`，该函数用于读取用户输入的5个字符串，并将它们存储在字符串数组`s`中。目前，第二个`for`循环的功能尚未实现。

--- c++/test.cpp
+++ c++/test.cpp
@@ -4,3 +4,13 @@ using namespace std;
 typedef long long ll;
 const int siz=1e5+15;
 
+int main(){
+    int n=5;
+    string s[5];
+    for(int i=0;i<n;i++){
+        cin>>s[i];
+    }
+    for(int i=0;i<n;i++){
+        
+    }
+}
