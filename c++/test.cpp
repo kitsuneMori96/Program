d18e246dc4675e9dd1a52d8a881490a1c2491e1c
kitsuneMori <2794637787@qq.com>
2025-12-18 14:14:22 +0800

    refactor: 移除九九乘法表代码
    将 `main` 函数中实现九九乘法表的代码移除。
    移除的内容包括外层循环从 1 到 9，内层循环从 1 到 i，并打印乘法表结果的相关代码。

--- c++/test.cpp
+++ c++/test.cpp
@@ -4,14 +4,3 @@ using namespace std;
 typedef long long ll;
 const int siz=1e5+15;
 
-
-int main() {    
-    for (int i = 1; i <= 9; i++) {
-        for (int j = 1; j <= i; j++) {
-            printf("%d*%d=%-2d  ", j, i, i * j);
-        }
-        printf("\n");
-    }
-    
-    return 0;
-}
