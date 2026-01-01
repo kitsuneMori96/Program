35210cbdbf653114964b67f28565cbb409fc6240
kitsuneMori <2794637787@qq.com>
2026-01-01 20:06:45 +0800

    feat(test.cpp): 添加查找并打印100以内质数的功能
    在`test.cpp`中，移除了原先的`#include <bits/stdc++.h>`和`using namespace std;`，并引入了`#include <stdio.h>`。
    添加了一个`main`函数，该函数用于查找并打印100以内的所有质数。
    使用`printf`函数输出质数，并在每行打印10个质数后换行。
    最后，输出100以内的质数总数。

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,6 +1,32 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
-typedef long long ll;
-const int siz=1e5+15;
-
+int main() {
+    printf("Prime numbers within 100:\n");
+    
+    int count = 0; 
+    
+    for (int i = 2; i <= 100; i++) {
+        int is_prime = 1;  
+        
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                is_prime = 0;  
+                break;
+            }
+        }
+        
+        if (is_prime) {
+            printf("%3d ", i);
+            count++;
+            
+            if (count % 10 == 0) {
+                printf("\n");
+            }
+        }
+    }
+    
+    printf("\n\nThere are %d prime numbers under 100.\n", count)
+;
+    
+    return 0;
+}
